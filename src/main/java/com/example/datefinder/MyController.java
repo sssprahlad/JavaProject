28883d7a3cf896714834aeeaf70f6907d2450975
sssprahlad <k.sssprahlad@gmail.com>
2025-06-05 16:21:58 +0000

    commit

---
+++ src/main/java/com/example/datefinder/MyController.java
@@ -0,0 +1,21 @@
+package com.example.datefinder;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class MyController {
+    
+    @GetMapping("/")
+    public String method(){
+        LocalDate today = LocalDate.now();
+        LocalDate addDays = today.plusDays(100);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        return addDays.format(formatter);
+    }
+
+}
